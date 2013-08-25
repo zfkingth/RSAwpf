e32fcf8d8c12f7d6148641f58b532bdf383fb45c
zfkingth <zfking@gmail.com>
2013-08-25 16:55:14 +0800

    add

---
+++ RSAwpf/MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace RSAwpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        RSAHelper helper = new RSAHelper();
+        UnicodeEncoding encoding = new UnicodeEncoding();
+        const string pubFileName = "public.xls";
+        const string priFileName = "private.xls";
+        const string encSuffix = "_encrypted";
+        const string decSuffix = "_decrypted";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            helper.Initial();
+            //save to file
+            string path = txtPath.Text.Trim();
+
+            if (System.IO.Directory.Exists(path) == false)
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+
+            using (System.IO.FileStream pfile = System.IO.File.Create(System.IO.Path.Combine(path, pubFileName)))
+            {
+
+                byte[] buffer = encoding.GetBytes(helper.PublicKey);
+                pfile.Write(buffer, 0, buffer.Length);
+                pfile.Close();
+            }
+
+            using (System.IO.FileStream sfile = System.IO.File.Create(System.IO.Path.Combine(path, priFileName)))
+            {
+
+                byte[] buffer = encoding.GetBytes(helper.PrivateKey);
+                sfile.Write(buffer, 0, buffer.Length);
+                sfile.Close();
+            }
+            tbResult.Text = "操作成功.";
+
+
+        }
+
+
+        /// <summary>
+        /// encrypt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            byte[] buffer = null;
+
+
+
+            using (System.IO.FileStream fs = System.IO.File.OpenRead(tbPublic.Text.Trim()))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(fs))
+                {
+                    buffer = br.ReadBytes((int)fs.Length);
+                    helper.PublicKey = encoding.GetString(buffer);
+
+
+                }
+            }
+
+            string encFilePath=tbEnc.Text.Trim();
+
+            //get file which is being encrypting
+            using (System.IO.FileStream fs = System.IO.File.OpenRead(encFilePath))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(fs))
+                {
+                    buffer = br.ReadBytes((int)fs.Length);
+
+
+
+                }
+            }
+
+            //write encrypted data to file
+            using (System.IO.FileStream fs = System.IO.File.OpenWrite(encFilePath + encSuffix))
+            {
+                using (System.IO.BinaryWriter bw = new System.IO.BinaryWriter(fs))
+                {
+
+                    int i = 0;
+                    //每次加密的block 大小
+                    int blocksize =210;//<size/8-41
+                    int currentSize;
+                    do
+                    {
+                        if (i + blocksize <= buffer.Length)
+                        {
+                            currentSize = blocksize;
+                        }
+                        else
+                        {
+                            currentSize = buffer.Length % blocksize;
+                        }
+                        byte[] blockBuffer = new byte[currentSize];
+                        Array.Copy(buffer, i, blockBuffer, 0, currentSize);
+                        var temp = helper.EncryptData(blockBuffer);
+
+                        bw.Write(temp);
+                        i += blocksize;
+                    } while (i < buffer.Length);
+
+                    bw.Close();
+                }
+
+                tbResult.Text = "操作完成";
+            }
+
+        }
+
+        /// <summary>
+        /// decrypt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            byte[] buffer = null;
+
+
+
+
+
+            using (System.IO.FileStream fs = System.IO.File.OpenRead(tbPrivate.Text.Trim()))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(fs))
+                {
+                    buffer = br.ReadBytes((int)fs.Length);
+                    helper.PrivateKey = encoding.GetString(buffer);
+
+
+                }
+            }
+
+
+
+            string decFilePath = tbDec.Text.Trim();
+
+            //get file which is being encrypting
+            using (System.IO.FileStream fs = System.IO.File.OpenRead(decFilePath))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(fs))
+                {
+                    buffer = br.ReadBytes((int)fs.Length);
+
+
+
+                }
+            }
+
+
+            using (System.IO.FileStream fs = System.IO.File.OpenWrite(decFilePath + decSuffix))
+            {
+                using (System.IO.BinaryWriter bw = new System.IO.BinaryWriter(fs))
+                {
+
+                    int i = 0;
+                    //每次加密的block 大小
+                    int blocksize = RSAHelper.KeySize / 8;
+                    int currentSize;
+                    do
+                    {
+                        if (i + blocksize <= buffer.Length)
+                        {
+                            currentSize = blocksize;
+                        }
+                        else
+                        {
+                            currentSize = buffer.Length % blocksize;
+                        }
+                        byte[] blockBuffer = new byte[currentSize];
+                        Array.Copy(buffer, i, blockBuffer, 0, currentSize);
+                        var temp = helper.DecryptData(blockBuffer);
+
+                        bw.Write(temp);
+                        i += blocksize;
+                    } while (i < buffer.Length);
+
+                    bw.Close();
+                }
+
+                tbResult.Text = "操作完成";
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ RSAwpf/RSAHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAwpf
+{
+    public class RSAHelper
+    {
+        public string PublicKey, PrivateKey;
+
+        public const int KeySize = 2048;
+        RSACryptoServiceProvider rsaProvider;
+        public void Initial()
+        {
+            //声明一个RSA算法的实例，由RSACryptoServiceProvider类型的构造函数指定了密钥长度为1024位
+            //实例化RSACryptoServiceProvider后，RSACryptoServiceProvider会自动生成密钥信息。
+            rsaProvider = new RSACryptoServiceProvider(KeySize);
+            //将RSA算法的公钥导出到字符串PublicKey中，参数为false表示不导出私钥
+            PublicKey = rsaProvider.ToXmlString(false);
+            //将RSA算法的私钥导出到字符串PrivateKey中，参数为true表示导出私钥
+            PrivateKey = rsaProvider.ToXmlString(true);
+        }
+
+        public int MaxEncryptSize
+        {
+            get
+            {
+               return rsaProvider.KeySize / 8 - 11;
+            }
+        }
+
+        public byte[] EncryptData(byte[] data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+            //将公钥导入到RSA对象中，准备加密；
+            rsa.FromXmlString(PublicKey);
+            //对数据data进行加密，并返回加密结果；
+            //第二个参数用来选择Padding的格式
+            return rsa.Encrypt(data, false);
+        }
+
+
+        //public byte[] EncryptDataWithPrivateKey(byte[] data)
+        //{
+        //    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+        //    //用私钥加密
+        //    rsa.FromXmlString(PrivateKey);
+        //    return rsa.Encrypt(data, false);
+        //}
+
+
+        //public byte[] DecryptDataWithPublicKey(byte[] data)
+        //{
+        //    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+        //    //将公钥导入RSA中，准备解密；
+        //    rsa.FromXmlString(PublicKey);
+        //    //对数据进行解密，并返回解密结果；
+        //    return rsa.Decrypt(data, false);
+        //}
+
+        public byte[] DecryptData(byte[] data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+            //将私钥导入RSA中，准备解密；
+            rsa.FromXmlString(PrivateKey);
+            //对数据进行解密，并返回解密结果；
+            return rsa.Decrypt(data, false);
+        }
+
+        public byte[] Sign(byte[] data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+            //导入私钥，准备签名
+            rsa.FromXmlString(PrivateKey);
+            //将数据使用MD5进行消息摘要，然后对摘要进行签名并返回签名数据
+            return rsa.SignData(data, "MD5");
+        }
+
+        public bool Verify(byte[] data, byte[] Signature)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(KeySize);
+            //导入公钥，准备验证签名
+            rsa.FromXmlString(PublicKey);
+            //返回数据验证结果
+            return rsa.VerifyData(data, "MD5", Signature);
+        }
+
+    }
+}
